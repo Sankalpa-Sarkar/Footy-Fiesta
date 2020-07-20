be73825534fafb93ce4a44dffd798325767216fa
Sankalpa Sarkar <47474648+Sankalpa-Sarkar@users.noreply.github.com>
2020-07-21 03:48:20 +0530

    Added game files to repository
    The relevant game files have been added to the repository, including the scripts, assets and mechanics.

---
+++ Assets/CreatedScripts/Football.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Football : MonoBehaviour
+{
+    public GameController Controller;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Goal")
+        {
+            Controller.IncrementScore();
+            
+        }
+    }
+}
